1fba9b1e8e1b0a7c438a5b65262394ce4175fc40
nimscoding <nhiem.codes@gmail.com>
2021-07-26 22:08:41 +1000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Recipe from "./Components/Recipe";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const API_ID = "6b24a020";
+  const API_KEY = "170d84afdfdfdd416551b814674f7090";
+
+  const [recipes, setRecipes] = useState([]);
+  const [recipeQuery, setRecipeQuery] = useState(" ");
+
+  const SearchRecipes = async () => {
+    const response = await axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeQuery}&app_id=${API_ID}&app_key=${API_KEY}`
+    );
+    setRecipes(response.data.hits);
+    console.log(response);
+    setRecipeQuery("");
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    SearchRecipes();
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRecipeQuery(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={SearchRecipes}>IngrediList</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Search Recipes"
+          onChange={onChange}
+          value={recipeQuery} //to clear input after search
+        ></input>
+        <input type="submit" value="search" />
+        {recipes !== [] &&
+          recipes.map((recipe) => <Recipe recipes={recipes} />)}
+      </form>
     </div>
   );
 }
---
+++ src/Components/Recipe.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recipe:React.FC<Props> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Recipe
